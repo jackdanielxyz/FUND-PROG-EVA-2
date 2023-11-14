e4aa169a3280299e9be2c25cb7cf111a11d193d6
Dany Perez (Ado's Lover) <143568493+jackdanielxyz@users.noreply.github.com>
2023-11-13 19:41:01 -0600

    Add files via upload

---
+++ EVA2_28_ARREGLOS_MDIM/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+                                //fila, columna
+        int[][] matriz = new int[3][];
+
+        matriz[0] = new int[10];
+        matriz[1] = new int[5];
+        matriz[2] = new int[3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (int) (Math.random()*100);
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("[" + matriz[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+}
