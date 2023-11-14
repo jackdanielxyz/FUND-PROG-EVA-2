477c2c31053bf046928489ad8fd74efcae652edf
Dany Perez (Ado's Lover) <143568493+jackdanielxyz@users.noreply.github.com>
2023-11-13 19:43:57 -0600

    Add files via upload

--- EVA2_28_ARREGLOS_MDIM/src/Main.java
+++ EVA2_28_ARREGLOS_MDIM/src/Main.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-                                //fila, columna
-        int[][] matriz = new int[3][];
-
-        matriz[0] = new int[10];
-        matriz[1] = new int[5];
-        matriz[2] = new int[3];
+                                //matriz: fila, columna
+        int[][] matriz = new int[5][10];
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
--- EVA2_29_ARREGLOS_MDIM_2/src/Main.java
+++ EVA2_29_ARREGLOS_MDIM_2/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //fila, columna
+        int[][] matriz = new int[3][];
+
+        matriz[0] = new int[10];
+        matriz[1] = new int[5];
+        matriz[2] = new int[3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (int) (Math.random()*100);
+            }
+        }
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("[" + matriz[i][j] + "]");
+            }
+            System.out.println();
+        }
     }
 }
